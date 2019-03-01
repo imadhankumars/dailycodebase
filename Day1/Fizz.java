aa23e0d8d8e0cd33090ca946dbedc3a06c62f19a
imadhankumars <39881064+imadhankumars@users.noreply.github.com>
2019-03-01 15:50:44 +0530

    Fizz.java

---
+++ Day1/Fizz.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+class Div
+{
+public static void main(String[] args)
+{
+int n;
+Scanner s=new Scanner(System.in);
+n=sc.nextInt();
+for(int i=1;i<=n:i++)
+{
+if(i%3==0 && i%5==0)
+{
+System.out.print("fizzbuzz");
+}
+else if(i%3==0)
+{
+System.out.print("fizz");
+}
+else if(i%5==0)
+{
+System.out.print("buzz");
+}
+}}
